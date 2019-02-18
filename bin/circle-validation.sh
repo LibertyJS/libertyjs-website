bd7a1fe99b07db75c4d5604994d323be828f7c05
Jonathan Belcher <belcherj@gmail.com>
2019-02-18 13:01:54 -0500

    Update circle-validation.sh

--- bin/circle-validation.sh
+++ bin/circle-validation.sh
@@ -11,10 +11,4 @@ if [ $? -ne 0 ]; then
 else
 	printf "\033[32mLinting Passed %s\033[0m\n"
 fi
-
-if ! $pass; then
-	printf "\n\033[41mVALIDATION FAILED:\033[0m Your HAS FAILED VALIDATION. Please fix the errors and try again.\n\n"
-	exit 1
-else
-	printf "\n\033[42mVALIDATION SUCCEEDED\033[0m\n\n"
-fi
+printf "\n\033[42mVALIDATION SUCCEEDED\033[0m\n\n"
