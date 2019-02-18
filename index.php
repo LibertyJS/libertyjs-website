5ca67c13521c58fd304e82e2dc4bd17981096906
Jonathan Belcher <jonathan.belcher@automattic.com>
2019-02-18 12:48:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,68 @@
-<?php
-/**
- * The main template file.
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,900');
+    html, body {
+      height:100%;
+      width: 100%;
+      margin: 0;
+      color: white;
+      background: black;
+      font-family: "Source Sans Pro";
+      font-weight: 400;
+      font-style: normal;
+      text-align: center;
+    }
+    body {
+      background:
+      linear-gradient(
+        rgba(0, 21, 104, 0.4),
+        rgba(0, 0, 0, 0.7),
+        rgba(0, 21, 104, 0.4)
+      ),
+      url('439.jpg');
+      background-size: cover;
+      background-position: center;
+    }
+    .overlay {
+      width:100%;
+      height:100%;
+      mix-blend-mode: multiply;
+      position: absolute;
+      background: rgba(0, 36, 178, 0.5);
+    }
+    #center {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 400px;
+    }
+    img {
+      width: 70%;
+    }
+    .dates {
+      font-size: 44px;
+    }
+    .venue {
+      font-size: 30px;
+    }
+  </style>
+  <title>Document</title>
+</head>
+<body>
+  <section id="main">
+    <div class="overlay"></div>
+    <div id="center">
+      <img src="logo-Dark-BG.png" alt="Liberty JS Logo">
 
-get_header();
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) {
-			get_template_part( 'loop' );
-		} else {
-			get_template_part( 'content', 'none' );
-		}
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-do_action( 'storefront_sidebar' );
-get_footer();
+      <p class="dates">October 24-25, 2019</p>
+      <p class="venue">National Museum of American Jewish History.</p>
+    </div>
+  </section>
+</body>
+</html>
